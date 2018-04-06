ba582af85e7f686cf25d8d46b9ce231ce3963cb1
luyanpu <lyp_usesr@163.com>
2018-04-06 16:46:44 +0800

    Signed-off-by: luyanpu <lyp_usesr@163.com> 提交初始

---
+++ movie_db.sql
@@ -0,0 +1,112 @@
+﻿# SQL Manager 2007 Lite for MySQL 4.4.2.1
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : movie_db
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE DATABASE `movie_db`
+    CHARACTER SET 'latin1'
+    COLLATE 'latin1_swedish_ci';
+
+USE `movie_db`;
+
+#
+# Structure for the `type` table : 
+#
+
+CREATE TABLE `type` (
+  `type_id` int(11) NOT NULL,
+  `type_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `user` table : 
+#
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(20) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` tinyint(15) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `avatar` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `favor` table : 
+#
+
+CREATE TABLE `favor` (
+  `favor_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  PRIMARY KEY (`favor_id`),
+  KEY `user_id` (`user_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `favor_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `favor_fk1` FOREIGN KEY (`type_id`) REFERENCES `type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `friend` table : 
+#
+
+CREATE TABLE `friend` (
+  `friend_id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid_1` int(11) NOT NULL,
+  `userid_2` int(11) NOT NULL,
+  PRIMARY KEY (`friend_id`),
+  KEY `userid_1` (`userid_1`),
+  KEY `userid_2` (`userid_2`),
+  CONSTRAINT `friend_fk1` FOREIGN KEY (`userid_2`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `friend_fk` FOREIGN KEY (`userid_1`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `movie` table : 
+#
+
+CREATE TABLE `movie` (
+  `movie_id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_name` varchar(20) NOT NULL,
+  `movie_summary` varchar(300) NOT NULL,
+  `movie_pic` varchar(40) NOT NULL,
+  `movie_type` int(11) NOT NULL,
+  PRIMARY KEY (`movie_id`),
+  KEY `movie_type` (`movie_type`),
+  CONSTRAINT `movie_fk` FOREIGN KEY (`movie_type`) REFERENCES `type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `movie_comment` table : 
+#
+
+CREATE TABLE `movie_comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `comment_text` varchar(300) DEFAULT NULL,
+  `comment_time` date DEFAULT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `user_id` (`user_id`),
+  KEY `movie_id` (`movie_id`),
+  CONSTRAINT `movie_comment_fk1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
+  CONSTRAINT `movie_comment_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
