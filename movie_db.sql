d49faefcc6d08279969fc00c21beb4c3898daf75
luyanpu <lyp_usesr@163.com>
2018-04-06 17:31:13 +0800

    Signed-off-by: luyanpu <lyp_usesr@163.com>

--- movie_db.sql
+++ movie_db.sql
@@ -1,112 +1,108 @@
-﻿# SQL Manager 2007 Lite for MySQL 4.4.2.1
-# ---------------------------------------
-# Host     : localhost
-# Port     : 3306
-# Database : movie_db
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : movie
+ Source Server Type    : MySQL
+ Source Server Version : 50615
+ Source Host           : localhost:3306
+ Source Schema         : movie_db
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ Target Server Type    : MySQL
+ Target Server Version : 50615
+ File Encoding         : 65001
 
-SET FOREIGN_KEY_CHECKS=0;
+ Date: 06/04/2018 17:29:19
+*/
 
-CREATE DATABASE `movie_db`
-    CHARACTER SET 'latin1'
-    COLLATE 'latin1_swedish_ci';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-USE `movie_db`;
-
-#
-# Structure for the `type` table : 
-#
-
-CREATE TABLE `type` (
-  `type_id` int(11) NOT NULL,
-  `type_name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `user` table : 
-#
-
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(20) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` tinyint(15) NOT NULL,
-  `birthday` date DEFAULT NULL,
-  `avatar` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `favor` table : 
-#
-
-CREATE TABLE `favor` (
+-- ----------------------------
+-- Table structure for favor
+-- ----------------------------
+DROP TABLE IF EXISTS `favor`;
+CREATE TABLE `favor`  (
   `favor_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `type_id` int(11) NOT NULL,
-  PRIMARY KEY (`favor_id`),
-  KEY `user_id` (`user_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `favor_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `favor_fk1` FOREIGN KEY (`type_id`) REFERENCES `type` (`type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`favor_id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `type_id`(`type_id`) USING BTREE,
+  CONSTRAINT `favor_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `favor_fk1` FOREIGN KEY (`type_id`) REFERENCES `type` (`type_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
-#
-# Structure for the `friend` table : 
-#
-
-CREATE TABLE `friend` (
+-- ----------------------------
+-- Table structure for friend
+-- ----------------------------
+DROP TABLE IF EXISTS `friend`;
+CREATE TABLE `friend`  (
   `friend_id` int(11) NOT NULL AUTO_INCREMENT,
   `userid_1` int(11) NOT NULL,
   `userid_2` int(11) NOT NULL,
-  PRIMARY KEY (`friend_id`),
-  KEY `userid_1` (`userid_1`),
-  KEY `userid_2` (`userid_2`),
-  CONSTRAINT `friend_fk1` FOREIGN KEY (`userid_2`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `friend_fk` FOREIGN KEY (`userid_1`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`friend_id`) USING BTREE,
+  INDEX `userid_1`(`userid_1`) USING BTREE,
+  INDEX `userid_2`(`userid_2`) USING BTREE,
+  CONSTRAINT `friend_fk1` FOREIGN KEY (`userid_2`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `friend_fk` FOREIGN KEY (`userid_1`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for the `movie` table : 
-#
-
-CREATE TABLE `movie` (
+-- ----------------------------
+-- Table structure for movie
+-- ----------------------------
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE `movie`  (
   `movie_id` int(11) NOT NULL AUTO_INCREMENT,
-  `movie_name` varchar(20) NOT NULL,
-  `movie_summary` varchar(300) NOT NULL,
-  `movie_pic` varchar(40) NOT NULL,
+  `movie_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `movie_summary` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `movie_pic` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `movie_type` int(11) NOT NULL,
-  PRIMARY KEY (`movie_id`),
-  KEY `movie_type` (`movie_type`),
-  CONSTRAINT `movie_fk` FOREIGN KEY (`movie_type`) REFERENCES `type` (`type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`movie_id`) USING BTREE,
+  INDEX `movie_type`(`movie_type`) USING BTREE,
+  CONSTRAINT `movie_fk` FOREIGN KEY (`movie_type`) REFERENCES `type` (`type_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for the `movie_comment` table : 
-#
-
-CREATE TABLE `movie_comment` (
+-- ----------------------------
+-- Table structure for movie_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `movie_comment`;
+CREATE TABLE `movie_comment`  (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `movie_id` int(11) DEFAULT NULL,
-  `comment_text` varchar(300) DEFAULT NULL,
-  `comment_time` date DEFAULT NULL,
-  PRIMARY KEY (`comment_id`),
-  KEY `user_id` (`user_id`),
-  KEY `movie_id` (`movie_id`),
-  CONSTRAINT `movie_comment_fk1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`),
-  CONSTRAINT `movie_comment_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `user_id` int(11) NULL DEFAULT NULL,
+  `movie_id` int(11) NULL DEFAULT NULL,
+  `comment_text` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `comment_time` date NULL DEFAULT NULL,
+  PRIMARY KEY (`comment_id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `movie_id`(`movie_id`) USING BTREE,
+  CONSTRAINT `movie_comment_fk1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `movie_comment_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
+-- ----------------------------
+-- Table structure for type
+-- ----------------------------
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE `type`  (
+  `type_id` int(11) NOT NULL,
+  `type_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`type_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone` int(11) NOT NULL,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` tinyint(15) NOT NULL,
+  `birthday` date NULL DEFAULT NULL,
+  `avatar` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
