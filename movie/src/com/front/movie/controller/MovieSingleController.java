650311235e3d2bc28fc5ddddca0c1b4fe47dc0b5
Lee MengMing <lmmmml54188@qq.com>
2018-04-10 21:36:55 +0800

    修改回滚后文件

--- movie/src/com/front/movie/controller/MovieSingleController.java
+++ movie/src/com/front/movie/controller/MovieSingleController.java
@@ -2,8 +2,7 @@ package com.front.movie.controller;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,40 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.front.movie.entity.Movie;
-=======
-=======
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-<<<<<<< HEAD
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
-=======
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
+
 import com.front.movie.service.MovieServiceImpl;
 
 @Controller
 public class MovieSingleController {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	
-	@Resource
-	private MovieServiceImpl msi;
-	
-	
-	//跳转电影详情页
-	@RequestMapping(value="movie-single/{id}",method=RequestMethod.POST)
-	public String movieSingle(@PathVariable int id,HttpServletRequest request){
-		msi.findMovieById(request, id);
-		return "movie-single";
-	}
-	
-	
-=======
-=======
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
 
 	@Resource
 	private MovieServiceImpl msi;
@@ -54,8 +28,5 @@ public class MovieSingleController {
 	public void ShowMovieSingle(HttpServletRequest request,@RequestParam("id") int id) {
 		this.msi.findMovieById(request, id);
 		}
-<<<<<<< HEAD
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
-=======
->>>>>>> 19bb0d9f9c5048a3e39fa3661500f748ff3aa747
+
 }
