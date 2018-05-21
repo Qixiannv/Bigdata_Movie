9e6c19db0e8922dd002a2a2a1ecadd7dcb26fc5e
porkeater <248207330@qq.com>
2018-05-21 10:25:36 +0800

    1

--- movie/src/com/front/movie/controller/MovieIndexShowController.java
+++ movie/src/com/front/movie/controller/MovieIndexShowController.java
@@ -5,6 +5,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.front.actor.service.ActorServiceImpl;
 import com.front.movie.service.MovieServiceImpl;
 
 
@@ -13,10 +15,13 @@ public class MovieIndexShowController {
 	
 	@Resource
 	private MovieServiceImpl msi;
+	@Resource
+	private ActorServiceImpl asi;
 	//主页轮播图
 	@GetMapping("/indexshow")
 	public String IndexShow(HttpServletRequest request) {
 		request.setAttribute("movielist", this.msi.searchAllMovie());
+		request.setAttribute("actorlist", this.asi.findActors());
 		return "/index";
 	}
 }
